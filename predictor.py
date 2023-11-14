46a28f0f118b188b4842588f805c857972637f83
kamilalfian <37233870+kamilalfian@users.noreply.github.com>
2023-11-14 08:12:00 +0700

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -17,7 +17,8 @@ st.title('Stock Prediction')
 #Fetching stock data
 user_input=st.text_input('Enter Stock Ticker (based on Yahoo Finance Website, ex: INTP.JK)','INTP.JK')
 start='2013-01-01'
-end=datetime.now(pytz.timezone('Asia/Jakarta')).strftime('%Y-%m-%d')
+end = (datetime.now(pytz.timezone('Asia/Jakarta')) + pd.offsets.BDay(1)).strftime('%Y-%m-%d')
+st.write(end)
 yfin.pdr_override()
 df=yfin.download(user_input,start,end)
 
